a91765dbd24b0e44fbdc739669a4fd8a6a7e5d2d
emiliqn101 <147400335+emiliqn101@users.noreply.github.com>
2023-10-18 13:37:47 +0300

    Create a simple page with input field

--- EncryptionVisualization/EncryptionVisualization/asgi.py
+++ EncryptionVisualization/EncryptionVisualization/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for CryptographyVisualization project.
+ASGI config for EncryptionVisualization project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CryptographyVisualization.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EncryptionVisualization.settings')
 
 application = get_asgi_application()
--- EncryptionVisualization/manage.py
+++ EncryptionVisualization/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CryptographyVisualization.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EncryptionVisualization.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
